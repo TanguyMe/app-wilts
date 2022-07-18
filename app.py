993b01851903c702992ce95b2b69631e1b162fd5
Tanguy Meyer <Tanguy.meyer67260@gmail.com>
2022-07-19 01:43:17 +0200

    Fix port

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flaskr import create_app
 from prediction.predict import cv_silhouette_scorer
 
 if __name__ == "__main__":
-    create_app().run(port=5001, debug=True)
+    create_app().run(port=8000, debug=True)
 
 # from flask import Flask
 #
